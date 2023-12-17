c785276bec292288bd2a2d713f0f7f5cbb12dd72
nathvirginia <148851437+nathvirginia@users.noreply.github.com>
2023-12-17 02:02:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let typingEffect = new Typed(".animation", {
-  strings: ["Front-End Developer", "Software Engineer"],
+  strings: [
+    "Front-End Developer",
+    "Software Engineer Student",
+    "Tech Enthusiast",
+    "Continuous Learner",
+    " UI/UX Enthusiast",
+    "Travel Enthusiast",
+    "Brazilian",
+  ],
   loop: true,
   typeSpeed: 100,
   backSpeed: 80,
